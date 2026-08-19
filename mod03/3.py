c1633dafc09eba458e1753785d2bfa54e4ec3bd8
xtofuub <99559348+xtofuub@users.noreply.github.com>
2026-08-19 11:33:58 +0300

    yeah buddy

---
+++ mod03/3.py
@@ -0,0 +1,6 @@
+kanta = float(input('syötä kanta: '))
+korkeus = float(input('syötä korkeus: '))
+piiri = 2*kanta + 2*korkeus
+print("Suorakulmion piiri on:", piiri)
+pinta_ala = kanta * korkeus
+print("Suorakulmion pinta-ala on:", pinta_ala)
