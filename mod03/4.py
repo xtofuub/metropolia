d6bc8cdb85e315dc975f66683171cb25d5061eaa
xtofuub <99559348+xtofuub@users.noreply.github.com>
2026-08-19 11:39:15 +0300

    boom

---
+++ mod03/4.py
@@ -0,0 +1,9 @@
+luku1 = float(input('syötä eka luku: '))
+luku2 = float(input('syötä toka luku: '))
+luku3 = float(input('syötä kolmas luku: '))
+summa = luku1 + luku2 + luku3
+print("Lukujen summa on:", summa)
+tulo = luku1 * luku2 * luku3
+print("Lukujen tulo on:", tulo)
+keskiarvo = summa / 3
+print("Lukujen keskiarvo on:", keskiarvo)
